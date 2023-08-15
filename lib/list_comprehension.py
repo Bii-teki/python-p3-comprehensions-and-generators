46822656fcf3d327471eac3778b12cf721fee65f
Geoffrey Bii <geoffrey.bii@student.moringaschool.com>
2023-08-15 23:01:36 +0300

    complete assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    
+   
+    even = [num for num in num_list if( num% 2 == 0)]   
+    return even
+
 
 def make_exclamation(sentence_list):
-    pass
+    empty = []
+    for sentence in sentence_list:
+        sentence1 = sentence + "!"
+        empty.append(sentence1)
+    return empty
