b90660c04b62e1b2ce7036a584bd1c723dc2ae11
Geoffrey Bii <geoffrey.bii@student.moringaschool.com>
2023-08-17 18:17:02 +0300

    update on assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,14 +2,23 @@
 
 def return_evens(num_list):
     
-   
+   # result = []
+   # for num in num_list:
+    #    if(num%2== 0):
+   #          result.append(num)
+
+   # return result        
+
+
     even = [num for num in num_list if( num% 2 == 0)]   
     return even
 
 
 def make_exclamation(sentence_list):
-    empty = []
-    for sentence in sentence_list:
-        sentence1 = sentence + "!"
-        empty.append(sentence1)
-    return empty
+    #empty = []
+    #for sentence in sentence_list:
+     #   sentence1 = sentence + "!"
+     #   empty.append(sentence1)
+
+    empty1 = [sentence + "!" for sentence in sentence_list]   
+    return empty1
